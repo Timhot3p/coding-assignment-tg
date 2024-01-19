ed126f4f8ab887ee67789037520b467b6999e001
Tim Groß <44930867+Timhot3p@users.noreply.github.com>
2024-01-19 03:14:27 +0100

    Fixed portfolio image
    displays image now, deleted comments

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -8,18 +8,20 @@ export default function PortfolioPage() {
       <div>On this page, I would like to show you some of my projects.</div>
 
       <div className="mt-5">
-        Here is a screenshot of my latest project, the PresseBox website (TODO:
-        fix image - see code):
-        {/* I want to use next image for this to display the following image URL */}
-        {/* Image URL: https://www.unn-online.de/wp-content/uploads/2022/11/produkte-pressebox-1024x676.png */}
+        Here is a screenshot of my latest project, the PresseBox website:
         <div className="border-1 p-2">
-          {/* <Image
-            hidden={true}
-            className="mt-5 border-1"
-            width={1024}
-            height={676}
-            alt="Screenshot of the PresseBox website"
-          /> */}
+          {
+            <Image
+              hidden={false}
+              className="mt-5 border-1"
+              width={1024}
+              height={676}
+              alt="Screenshot of the PresseBox website"
+              src={
+                "https://www.unn-online.de/wp-content/uploads/2022/11/produkte-pressebox-1024x676.png"
+              }
+            />
+          }
         </div>
       </div>
     </div>
